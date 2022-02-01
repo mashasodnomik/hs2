71305319ceada5de8cc78fad1317486ae8f9a63f
Roman M. Gerasimov <romagrizly@gmail.com>
2022-02-01 13:47:12 +0500

    commit 2

--- main.py
+++ main.py
@@ -37,5 +37,46 @@ def return_sample_page():
                 </html>"""
 
 
+@app.route('/bootstrap_sample')
+def bootstrap():
+    return '''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Привет, Яндекс!</h1>
+                    <div class="alert alert-primary" role="alert">
+                      А мы тут компонентами Bootstrap балуемся
+                    </div>
+                  </body>
+                </html>'''
+
+
+@app.route('/greeting/<username>')
+def greeting(username):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Привет, {username}</title>
+                  </head>
+                  <body>
+                    <h1>Привет, {username}!</h1>
+                  </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
