0aa735caaa705be78cba0f6b83bb5e27d7aef6b3
Roman M. Gerasimov <romagrizly@gmail.com>
2022-02-01 13:53:14 +0500

    commit 3

--- main.py
+++ main.py
@@ -78,5 +78,26 @@ def greeting(username):
                 </html>'''
 
 
+@app.route('/two_params/<username>/<int:number>')
+def two_params(username, number):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Пример с несколькими параметрами</title>
+                  </head>
+                  <body>
+                    <h2>{username}</h2>
+                    <div>Это первый параметр и его тип: {str(type(username))[1:-1]}</div>
+                    <h2>{number}</h2>
+                    <div>Это второй параметр и его тип: {str(type(number))[1:-1]}</div>
+                  </body>
+                </html>'''
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
