a8b81875602b1fbdf089627fe72524612ccf56d0
Roman Gersaimov <romagrizly@gmail.com>
2022-02-05 11:47:03 +0500

    add jinja templates

--- web2/main.py
+++ web2/main.py
@@ -20,10 +20,15 @@ def odd_even(number):
 
 @app.route('/news')
 def news():
-    with open("news.json", "r", encoding="utf8") as f:
+    with open("news.json", "r", encoding="utf-8") as f:
         news_list = json.load(f)
     print(news_list)
     return render_template('news.html', news=news_list)
+
+
+@app.route("/queue")
+def queue():
+    return render_template("queue.html", title="Очередь")
 
 
 if __name__ == '__main__':
