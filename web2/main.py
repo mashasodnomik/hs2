91004a26c424b3cf817c487a3c7fdfeacf5a4b65
Roman Gersaimov <romagrizly@gmail.com>
2022-02-05 11:17:03 +0500

    create lesson web 2

---
+++ web2/main.py
@@ -0,0 +1,30 @@
+import json
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = "Ученик Яндекс.Лицея"
+    return render_template('index.html', title='Домашняя страница',
+                           username=user)
+
+
+@app.route('/odd_even/<int:number>')
+def odd_even(number):
+    return render_template('odd_even.html', number=number)
+
+
+@app.route('/news')
+def news():
+    with open("news.json", "rt", encoding="utf8") as f:
+        news_list = json.load(f)
+    print(news_list)
+    return render_template('news.html', news=news_list)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
