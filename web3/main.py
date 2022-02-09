5a47aa39a12a844f268296b552acac3f514d05b3
Roman Gersaimov <romagrizly@gmail.com>
2022-02-09 13:32:14 +0500

    wtf: full lesson

--- web3/main.py
+++ web3/main.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, url_for, redirect
 from dotenv import load_dotenv
 import os
 from forms.loginform import LoginForm
+from forms.crash_loginform import CrashLoginForm
 
 
 load_dotenv()
@@ -18,6 +19,14 @@ def login():
     if form.validate_on_submit():
         return redirect('/success')
     return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route("/crash_login", methods=['GET', 'POST'])
+def crash_login():
+    form = CrashLoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('crash_login.html', title='Аварийный доступ', form=form)
 
 
 if __name__ == '__main__':
